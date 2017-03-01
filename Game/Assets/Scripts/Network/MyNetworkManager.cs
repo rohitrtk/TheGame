4e5edb6cc85d6521a3aa80cc899226fe851f5ec9
rohitrtk <rohit_rtk@hotmail.com>
2017-03-01 18:33:24 -0500

    Continue with multiplayer setup

---
+++ Game/Assets/Scripts/Network/MyNetworkManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager
+{
+     public void StartupHost()
+    {
+        SetPort();
+        NetworkManager.singleton.StartHost();
+    }
+	
+    public void JoinGame()
+    {
+        SetIPAdress();
+        SetPort();
+        NetworkManager.singleton.StartClient();
+    }
+
+    private void SetPort()
+    {
+        NetworkManager.singleton.networkPort = 7777;
+    }
+
+    private void SetIPAdress()
+    {
+        var ipAdress = GameObject.Find("InputIP").transform.FindChild("Text").GetComponent<Text>().text;
+        NetworkManager.singleton.networkAddress = ipAdress;
+    }
+}
