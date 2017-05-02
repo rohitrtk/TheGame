22fc3d49557f0d3a4a2a6fde653441fb92259158
rtk <rohit_rtk@hotmail.com>
2017-05-02 11:42:13 -0400

    Attempted to add an outline shader for mouse hover

---
+++ Game/Assets/Scripts/Object/OutlineHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutlineHandler : MonoBehaviour
+{
+    Renderer r;
+
+    private void Start()
+    {
+        r = GetComponent<Renderer>();
+    }
+
+    private void OnMouseEnter()
+    {
+        r.material.shader = Shader.Find("Self-Illumin/Outlined Diffuse");
+    }
+
+    private void OnMouseExit()
+    {
+        r.material.shader = Shader.Find("Diffuse");
+    }
+
+}
